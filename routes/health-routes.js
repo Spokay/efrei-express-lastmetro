7c8e342edc0cb455ae0d8f260d410cb2b4b0860c
Spokay <hugobressange@gmail.com>
2025-09-17 14:41:38 +0200

    feat: ajout d'un endpoint pour vérifier la connexion a la BDD

--- routes/health-routes.js
+++ routes/health-routes.js
@@ -1,9 +1,21 @@
 
-
 const healthRoutes = require("express").Router();
+const { sequelize } = require("../database/database");
 
 healthRoutes.get("/", (req, res) => {
     res.json({ status: "ok" });
+});
+
+
+healthRoutes.get("/db", (req, res) => {
+    sequelize.authenticate()
+        .then(() => {
+            res.status(200).json({ database: "Connected" });
+        })
+        .catch(err => {
+            console.error("Database connection error:", err);
+            res.status(500).json({ database: "Unreachable", error: err.message });
+        });
 });
 
 module.exports = healthRoutes;
